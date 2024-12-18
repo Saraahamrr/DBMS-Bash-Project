cfe3f9ae4e31bae37ae6344a16ce6b33f179f49c
MahmoudNasrZ <Mahmoudn.zalabany256@gmail.com>
2024-12-18 17:36:10 +0200

    This is the second Update after fix

--- Scripts/connectDB.sh
+++ Scripts/connectDB.sh
@@ -2,22 +2,14 @@
 # shopt -s extglob
 #REGEX Needed
 function selectDB() {
-<<<<<<< HEAD
-    echo "Enter the name of the database to select:"
-=======
     echo "Enter the name of the database to Connect:"
->>>>>>> af50f2c (DBMS Update 2)
     read -r dbName
     if [[ -e ~/DBMS-Bash-Project/DataBase/$dbName ]] && [[ -d ~/DBMS-Bash-Project/DataBase/$dbName ]]; then
         echo "Database '$dbName' selected."
-<<<<<<< HEAD
-        cd ~/DBMS-Bash-Project/DataBase/$dbName 2>>/dev/null
-=======
         cd ~/DBMS-Bash-Project/DataBase/"${dbName}/" 2>>/dev/null
         echo "${PWD}"
         . ~/DBMS/Scripts/./tableMenu.sh
         
->>>>>>> af50f2c (DBMS Update 2)
     else
         echo -e "Database '$dbName' does not exist \nDo you want to Create DB?"
         read answer
